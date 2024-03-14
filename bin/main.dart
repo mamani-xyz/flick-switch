bdbc802207770e103cb90189e3c00fbddf985a0d
Mamani <mamani.developer@gmail.com>
2024-03-14 16:36:20 -0400

    Implement the function flick switch

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flick_switch/flick_switch.dart';
+
 void main() {
-  print('Hello, World!');
+  print('Enter a list of String');
+  List<String> list =
+      json.decode(stdin.readLineSync().toString()).cast<String>();
+  print(flickSwitch(list));
 }
