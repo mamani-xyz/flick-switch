ed338c3e0809f572d2819096c3c9a47e12aea93b
Mamani <mamani.developer@gmail.com>
2024-03-17 17:03:57 -0400

    Use arrow function

--- test/flick_switch_test.dart
+++ test/flick_switch_test.dart
@@ -3,17 +3,19 @@ import "package:flick_switch/flick_switch.dart";
 
 void main() {
   group('$flickSwitch()', () {
-    test("to ['code', 'flick', 'coin', 'wars']", () {
-      expect(flickSwitch(['code', 'flick', 'coin', 'wars']),
-          equals([true, false, false, false]));
-    });
-    test("to ['flick', 'chocolate', 'adventure', 'sunshine']", () {
-      expect(flickSwitch(['flick', 'chocolate', 'adventure', 'sunshine']),
-          equals([false, false, false, false]));
-    });
-    test("to ['bicycle', 'jarmony', 'flick', 'sheep', 'flick']", () {
-      expect(flickSwitch(['bicycle', 'jarmony', 'flick', 'sheep', 'flick']),
-          equals([true, true, false, false, true]));
-    });
+    test(
+        "to ['code', 'flick', 'coin', 'wars']",
+        () => expect(flickSwitch(['code', 'flick', 'coin', 'wars']),
+            equals([true, false, false, false])));
+    test(
+        "to ['flick', 'chocolate', 'adventure', 'sunshine']",
+        () => expect(
+            flickSwitch(['flick', 'chocolate', 'adventure', 'sunshine']),
+            equals([false, false, false, false])));
+    test(
+        "to ['bicycle', 'jarmony', 'flick', 'sheep', 'flick']",
+        () => expect(
+            flickSwitch(['bicycle', 'jarmony', 'flick', 'sheep', 'flick']),
+            equals([true, true, false, false, true])));
   });
 }
